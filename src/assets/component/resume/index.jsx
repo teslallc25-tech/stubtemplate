3035b51417d3cec66afd5d3d4a678c02ee6de1ba
teslallc25-tech <teslallc25@gmail.com>
2026-03-10 08:03:00 -0400

    'updated'

--- src/assets/component/resume/index.jsx
+++ src/assets/component/resume/index.jsx
@@ -6,16 +6,17 @@ function Resume() {
       <div className="w-[850px] bg-white border shadow-lg">
 
         {/* HEADER */}
-        <div className="bg-blue-600 text-white flex justify-between p-4">
+        <div className="bg-orange-600 text-white flex justify-between p-4">
           <div className="text-left">
-            <h1 className="text-lg font-bold">The Fresh Market</h1>
-            <p className="text-sm">13493 Atlantic Blvd, Jacksonville, FL 32225</p>
+            <h1 className="text-lg font-bold">Gyro 365</h1>
+            <p className="text-sm">11101-1 Old St Augustine Rd</p>
+            <p className="text-sm">Jacksonville, FL 32257</p>
           </div>
 
           <div className="text-left">
             <h2 className="text-lg font-semibold">EARNINGS STATEMENT</h2>
-            <p className="text-sm">Pay Date: 01/26/2026</p>
-            <p className="text-sm">Reporting Period: 12/24/2025 - 01/23/2026</p>
+            <p className="text-sm">Pay Date: 01/30/2026</p>
+            <p className="text-sm">Reporting Period: 12/28/2025 - 01/27/2026</p>
           </div>
         </div>
 
@@ -23,22 +24,22 @@ function Resume() {
         <div className="flex justify-between p-4 border-b text-sm">
 
           <div className="text-left">
-            <p><b>Employee</b></p>
-            <p>Donald Hicks</p>
-            <p className="text-xs">574 Vikings Ln</p>
-            <p className="text-xs">Atlantic Beach FL 32233-4151</p>
+            <p className="font-semibold">Employee</p>
+            <p>William Wedebrock</p>
+            <p className="text-xs">635 Sailfish Dr E</p>
+            <p className="text-xs">Atlantic Beach FL 32233-4200</p>
           </div>
 
           <div className="flex flex-col justify-between text-sm">
 
-            <div className="flex justify-between w-32">
+            <div className="flex justify-between w-40">
               <span className="font-semibold">Employee No.</span>
-              <span>8</span>
+              <span>1</span>
             </div>
 
-            <div className="flex justify-between w-32 mt-1">
+            <div className="flex justify-between w-40 mt-1">
               <span className="font-semibold">SSN</span>
-              <span>XXX-XX-6385</span>
+              <span>XXX-XX-9658</span>
             </div>
 
           </div>
@@ -52,7 +53,7 @@ function Resume() {
           {/* INCOME */}
           <div className="border">
 
-            <div className="bg-blue-500 text-white font-semibold p-2 text-left">
+            <div className="bg-orange-500 text-white font-semibold p-2 text-left">
               Income
             </div>
 
@@ -71,16 +72,16 @@ function Resume() {
 
                 <tr>
                   <td className="p-2 text-left">Gross Earnings</td>
-                  <td className="p-2 text-right">15.24</td>
-                  <td className="p-2 text-right">190</td>
-                  <td className="p-2 text-right">2,895.60</td>
+                  <td className="p-2 text-right">14.30</td>
+                  <td className="p-2 text-right">80</td>
+                  <td className="p-2 text-right">1,144.00</td>
                 </tr>
 
                 <tr className="border-t font-semibold">
                   <td className="p-2 text-left">YTD Gross</td>
                   <td></td>
                   <td></td>
-                  <td className="p-2 text-right">2,895.60</td>
+                  <td className="p-2 text-right">1,144.00</td>
                 </tr>
 
               </tbody>
@@ -93,20 +94,18 @@ function Resume() {
           {/* DEDUCTIONS */}
           <div className="border">
 
-            <div className="bg-blue-500 text-white font-semibold p-2">
+            <div className="bg-orange-500 text-white font-semibold p-2 text-left">
               Deductions
             </div>
 
             <table className="w-full text-sm">
 
               <thead className="bg-gray-100">
-
                 <tr>
                   <th className="p-2 text-left">Deductions</th>
                   <th className="p-2 text-right">Total</th>
                   <th className="p-2 text-right">YTD Total</th>
                 </tr>
-
               </thead>
 
               <tbody>
@@ -121,32 +120,32 @@ function Resume() {
 
                 <tr>
                   <td className="p-2 text-left">FICA - Medicare</td>
-                  <td className="p-2 text-right">41.99</td>
-                  <td className="p-2 text-right">41.99</td>
+                  <td className="p-2 text-right">16.59</td>
+                  <td className="p-2 text-right">16.59</td>
                 </tr>
 
                 <tr>
                   <td className="p-2 text-left">FICA - Social Security</td>
-                  <td className="p-2 text-right">179.53</td>
-                  <td className="p-2 text-right">179.53</td>
+                  <td className="p-2 text-right">70.93</td>
+                  <td className="p-2 text-right">70.93</td>
                 </tr>
 
                 <tr>
                   <td className="p-2 text-left">Federal Tax</td>
-                  <td className="p-2 text-right">190.64</td>
-                  <td className="p-2 text-right">190.64</td>
+                  <td className="p-2 text-right">0.00</td>
+                  <td className="p-2 text-right">0.00</td>
                 </tr>
 
                 <tr>
                   <td className="p-2 text-left">FL STATE TAX W/H</td>
-                  <td></td>
-                  <td></td>
+                  <td className="p-2 text-right">0.00</td>
+                  <td className="p-2 text-right">0.00</td>
                 </tr>
 
                 <tr className="border-t font-semibold">
                   <td className="p-2 text-left">Total Deductions</td>
-                  <td className="p-2 text-right">412.16</td>
-                  <td className="p-2 text-right">412.16</td>
+                  <td className="p-2 text-right">87.52</td>
+                  <td className="p-2 text-right">87.52</td>
                 </tr>
 
               </tbody>
@@ -159,22 +158,22 @@ function Resume() {
 
 
         {/* TOTALS */}
-        <div className="border-t grid grid-cols-3 p-4 text-sm">
+        <div className="border-t grid grid-cols-3 gap-6 p-4 text-sm">
 
-          <div>
+          <div className="text-left">
             <p className="font-semibold">YTD Gross</p>
-            <p>2,895.60</p>
+            <p>1,144.00</p>
           </div>
 
-          <div>
+          <div className="text-left">
             <p className="font-semibold">YTD Deductions</p>
-            <p>412.16</p>
+            <p>87.52</p>
           </div>
 
           <div className="text-right">
             <p className="font-semibold">YTD Net Pay</p>
             <p className="text-xl font-bold text-green-600">
-              2,483.44
+              1,056.48
             </p>
           </div>
 
@@ -186,18 +185,18 @@ function Resume() {
 
           <div className="flex justify-between">
             <span className="font-semibold">Total</span>
-            <span>2,895.60</span>
+            <span>1,144.00</span>
           </div>
 
           <div className="flex justify-between">
             <span className="font-semibold">Deductions</span>
-            <span>412.16</span>
+            <span>87.52</span>
           </div>
 
           <div className="flex justify-between mt-2">
             <span className="font-semibold">Net Pay</span>
             <span className="text-xl font-bold text-green-600">
-              2,483.44
+              1,056.46
             </span>
           </div>
 
